a73ae259295f8442ce20375a2b3fb7e78ccf025f
itdboy <itdboy@gmail.com>
2023-11-01 23:12:21 +0700

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
-  return <p>Welcome To ProShop123</p>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>;
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  );
 };
 
 export default App;
